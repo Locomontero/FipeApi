70c814de9687a6c32d770a8bf9f5700d458d3b0a
Patrick Monteiro <locomontero33@hotmail.com>
2024-12-16 14:45:43 -0300

    FipeClient versionF

--- FipeApi/src/main/java/com/fipeapi1/services/FipeClient.java
+++ FipeApi/src/main/java/com/fipeapi1/services/FipeClient.java
@@ -11,23 +11,19 @@ import java.util.Map;
 @RegisterRestClient(baseUri = "https://parallelum.com.br/fipe/api/v1")
 public interface FipeClient {
 
-    // Endpoint para obter as marcas dos veículos
     @GET
     @Path("/carros/marcas")
     List<Map<String, String>> obterMarcas();
 
-    // Endpoint para obter os modelos de um veículo a partir da marca
     @GET
     @Path("/carros/marcas/{codigoMarca}/modelos")
     Map<String, Object> obterModelos(@PathParam("codigoMarca") String codigoMarca);
 
-    // Endpoint para obter os anos de um modelo específico
     @GET
     @Path("/carros/marcas/{codigoMarca}/modelos/{codigoModelo}/anos")
     List<Map<String, String>> obterAnos(@PathParam("codigoMarca") String codigoMarca,
                                         @PathParam("codigoModelo") String codigoModelo);
 
-    // Endpoint para obter o preço de um modelo, ano e marca
     @GET
     @Path("/carros/marcas/{codigoMarca}/modelos/{codigoModelo}/anos/{codigoAno}")
     Map<String, Object> obterPreco(@PathParam("codigoMarca") String codigoMarca,
